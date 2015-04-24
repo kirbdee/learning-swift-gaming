45cc1f24196829d70080b3706a03e86b7f134981
Kirby Domingo <kirbdee@kirbdee-MBP.local>
2015-04-23 18:57:00 -0700

    Added assets to project. Adjust View and Controller to display background.

--- Swiftris/Swiftris/GameScene.swift
+++ Swiftris/Swiftris/GameScene.swift
@@ -9,8 +9,26 @@
 import SpriteKit
 
 class GameScene: SKScene {
-
+    
+    required init(code aDecoder: NSCoder) {
+        fatalError("NSCoder not supported")
+    }
    
+    override init(size: CGSize) {
+        super.init(size: size)
+        
+        anchorPoint = CGPoint(x: 0, y: 1.0)
+        
+        let background = SKSpriteNode(imageNamed: "background")
+        background.position = CGPoint(x: 0, y: 0)
+        background.anchorPoint = CGPoint(x: 0, y: 1.0)
+        addChild(background)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
     }

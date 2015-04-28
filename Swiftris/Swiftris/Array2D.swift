8ad9742e63d541e3c5fbf34b4902c1ac418189a3
Kirby Domingo <kirbdee@kirbdee-MBP.local>
2015-04-27 19:16:15 -0700

    Added Array2D.swift file

---
+++ Swiftris/Swiftris/Array2D.swift
@@ -0,0 +1,36 @@
+//
+//  Array2D.swift
+//  Swiftris
+//
+//  Created by Kirby Domingo on 4/27/15.
+//  Copyright (c) 2015 Kirbdee. All rights reserved.
+//
+
+import Foundation
+
+// #1
+class Array2D<T> {
+    let columns: Int
+    let rows: Int
+    
+// #2
+    var array: Array<T?>
+    
+    init(columns: Int, rows: Int) {
+        self.columns = columns
+        self.rows = rows
+        
+// #3
+        array = Array<T?>(count:rows * columns, repeatedValue: nil)
+    }
+    
+// #4
+    subscript(column: Int, row: Int) -> T? {
+        get {
+            return array[(row * columns) + column]
+        }
+        set(newValue) {
+            array[(row * columns) + column] = newValue
+        }
+    }
+}
